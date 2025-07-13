76052e155a4973cf5b44baf3594c69e013fd4940
fadeldnswr <fadel.achmad@ui.ac.id>
2025-07-13 10:20:24 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SimulationPage from './pages/SimulationPage'
+
+function App() {
+
+  return (
+    <div>
+      <SimulationPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/SimulationPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SimulationPage = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to the Main Page
+      </h1>
+    </div>
+  )
+}
+
+export default SimulationPage;
